a7b7d184804981aba0d501b3f6dce6ccf47fa908
mercan <samuele.mercan@epfl.ch>
2018-10-31 22:59:49 +0100

    PCA and sequential forward selection

--- Project1/week4_task1_samuele.m
+++ Project1/week4_task1_samuele.m
@@ -27,7 +27,15 @@ PCAScoreDataCov = cov(score.');
 figure(2);
 image(PCAScoreDataCov,'CDataMapping','scaled');
 
+%%
 %We compare the variances (diagonal elements) as ratios
 VariancesRatios = diag(PCAScoreDataCov)./diag(trainDataCov);
 figure(3);
 plot(VariancesRatios);
+
+CovariancesRatios = PCAScoreDataCov./trainDataCov;
+image(CovariancesRatios,'CDataMapping','scaled');
+
+%% Choosing the number of PCs
+CumulativeVariance = cumsum((variance)/sum(variance));
+image(CovariancesRatios,'CDataMapping','scaled');
---
+++ Project1/week4_task2_samuele.m
@@ -0,0 +1,20 @@
+%% LOADING DATA & VARS
+
+loadingInVars_pietro;
+
+%%%%%%%%% LDA/QDA classifiers %%%%%%%%%%%
+
+%% Sequential forward selection
+
+fun = @(xT,yT,xt,yt) length(yt)*(immse(yt,predict(fitcdiscr(xT,yT,...
+    'discrimtype', 'diaglinear'), xt)));
+
+opt = statset('Display','iter','MaxIter',100);
+
+kfold=10;
+CrossValidationPartition = cvpartition(nObservations,'KFold',kfold);
+
+[sel,hst] = sequentialfs(fun,trainData,trainLabels,...
+    'cv',CrossValidationPartition,'options',opt);
+
+hst.Crit(end)
