a717e44fcbed1d5ab2daa0bd75311d1c41d763fc
mercan <samuele.mercan@epfl.ch>
2018-11-11 15:29:28 +0100

    Update Project1/week4_task2_samuele.m

--- Project1/week4_task2_samuele.m
+++ Project1/week4_task2_samuele.m
@@ -7,15 +7,17 @@ loadingInVars_pietro;
 %% Sequential forward selection
 %TODO ADD ERROR FUNCTION 
 
-fun = @(xT,yT,xt,yt) length(yt)*(immse(yt,predict(fitcdiscr(xT,yT,...
+tic();
+fun = @(xT,yT,xt,yt) length(yt)*(findCEByLabels(yt,predict(fitcdiscr(xT,yT,...
     'discrimtype', 'diaglinear'), xt)));
 
 opt = statset('Display','iter','MaxIter',100);
 
-kfold=10;
+kfold=3;
 CrossValidationPartition = cvpartition(nObservations,'KFold',kfold);
 
 [sel,hst] = sequentialfs(fun,trainData,trainLabels,...
     'cv',CrossValidationPartition,'options',opt);
 
 hst.Crit(end)
+toc();
